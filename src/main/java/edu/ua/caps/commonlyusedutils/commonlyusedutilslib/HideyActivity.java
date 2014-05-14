dd930fd22d21104f84606da99455a1240a4008d0
Aaron Fleshner <adfleshner@gmail.com>
2014-05-14 18:43:02 -0500

    Create HideyActivity.java
    Added immersive mode activity like called HideyActivity. To use add edu.ua.caps.commonlyuseduitls.commonlyusedutilslin.HideyAcitvity to your Manifest and then extend it.

---
+++ src/main/java/edu/ua/caps/commonlyusedutils/commonlyusedutilslib/HideyActivity.java
@@ -0,0 +1,20 @@
+public class HideyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    public void onWindowFocusChanged(boolean hasfocus){
+        super.onWindowFocusChanged(hasfocus);
+        if(hasfocus){
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
+}
