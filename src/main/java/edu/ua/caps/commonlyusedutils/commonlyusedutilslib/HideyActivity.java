04f08f7c64f96d4b8c2820f380f68f70c9580983
Aaron Fleshner <adfleshner@gmail.com>
2014-05-16 11:26:57 -0500

    Update HideyActivity.java

--- src/main/java/edu/ua/caps/commonlyusedutils/commonlyusedutilslib/HideyActivity.java
+++ src/main/java/edu/ua/caps/commonlyusedutils/commonlyusedutilslib/HideyActivity.java
@@ -3,6 +3,7 @@ package edu.ua.caps.commonlyusedutils.commonlyusedutilslib;
 import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 public class HideyActivity extends Activity {
 
@@ -10,17 +11,22 @@ public class HideyActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+    @TargetApi(11)
     @Override
-    public void onWindowFocusChanged(boolean hasfocus){
+    public void onWindowFocusChanged(boolean hasfocus) {
         super.onWindowFocusChanged(hasfocus);
-        if(hasfocus){
-            getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        if (hasfocus) {
+
+            if (CommonlyUsedUtils.isAndroidVersionHigherThanOrEqualTo(11)) {
+                getWindow().getDecorView().setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                );
+            }
         }
     }
 }
